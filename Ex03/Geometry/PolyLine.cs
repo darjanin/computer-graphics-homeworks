0d46922162cce4320da302d7e8032296876da24e
Milan Darjanin <me@milandarjanin.com>
2014-10-13 10:25:09 -0700

    Fixed version
    Removed no important code lines.

--- Ex03/Geometry/PolyLine.cs
+++ Ex03/Geometry/PolyLine.cs
@@ -40,17 +40,14 @@ namespace CG1.Ex03.Geometry
         public void Draw(Graphics g)
         {
             Vertex p0 = null;
+            double vectorLenght = 40;
 
-            //ToDo: Create a transform matrix. This matrix should include all transformations you applied on this polyline. 
-            //      Use Pivot, OrientationZDeg and Scale
+            // Transform matrix applied to all points
             Matrix44 transformMatrix = Matrix44.Translate(new Vector4(Pivot.X, Pivot.Y, 0, 1)) * Matrix44.RotateZ(OrientationZDeg) * Matrix44.Scale(Scale);
             
             //ToDo: Apply this new transform matrix to pivot axises
-            Vector4 xAxis = new Vector4(40, 0, 0, 1);
-            Vector4 yAxis = new Vector4(0, 40, 0, 1);
-            
-            xAxis = transformMatrix * xAxis;
-            yAxis = transformMatrix * yAxis;
+            Vector4 xAxis = transformMatrix * (new Vector4(vectorLenght, 0, 0, 1));
+            Vector4 yAxis = transformMatrix * (new Vector4(0, vectorLenght, 0, 1));
 			
             //Draw a polyline pivot and two axises in X and Y direction.
             g.DrawLine(Pens.Red, (float)Pivot.X, (float)Pivot.Y, (float)xAxis.X, (float)xAxis.Y);
@@ -62,16 +59,8 @@ namespace CG1.Ex03.Geometry
             {	
             	
             	Vertex p1 = new Vertex();
-            	p1.Position.X = v.Position.X;
-            	p1.Position.Y = v.Position.Y;
+            	p1.Position = transformMatrix * v.Position;
 
-                //ToDo: Use transform matrix to transform position of each polyline's vertex
-
-                Vector4 tmp = transformMatrix * p1.Position;
-                
-                p1.Position.X = tmp.X;
-                p1.Position.Y = tmp.Y;
-                
                 if (p0 != null)
                     g.DrawLine(Pens.Black, (float)p0.Position.X, (float)p0.Position.Y, (float)p1.Position.X, (float)p1.Position.Y);
                 g.FillEllipse(Brushes.Black, (float)p1.Position.X - 4, (float)p1.Position.Y - 4, 8, 8);
@@ -94,20 +83,22 @@ namespace CG1.Ex03.Geometry
             //ToDo: Change a position of vertex v to a position of local polyline vertex.
             //      Vertex v is a local vertex of polyline. And polyline is global object of Form1.
             //      So what will be a position of such vertex?
-			v.Position.X = point.X - Pivot.X;
-			v.Position.Y = point.Y - Pivot.Y;			
-			
+            
+			Matrix44 transformMatrix = Matrix44.Scale(new Vector4(1/Scale.X, 1/Scale.Y, 0, 0)) * Matrix44.RotateZ(-OrientationZDeg) * Matrix44.Translate(new Vector4(-Pivot.X, -Pivot.Y, 0, 1));
+			v.Position = transformMatrix * point;
+
             Vertices.Add(v);
         }
 
         public void TrySelect(Vector4 point)
         {
-            // Ttest if clicked point is near by polyline pivot
+            // Test if clicked point is near by polyline pivot
             // Try to do this with vectors.
             // If true then set IsSelected.
-            int threshold = 6;
-            if (Pivot.X > point.X - threshold && Pivot.X < point.X + threshold &&
-                Pivot.Y > point.Y - threshold && Pivot.Y < point.Y + threshold) {
+            int pointRadius = 6;
+            if (Pivot.X > point.X - pointRadius && Pivot.X < point.X + pointRadius &&
+                Pivot.Y > point.Y - pointRadius && Pivot.Y < point.Y + pointRadius) 
+            {
             	IsSelected = true;
             }
         }
