325b165c3d18bb5b5e558da01e0f4a200174ac49
Milan Darjanin <me@milandarjanin.com>
2014-10-20 15:54:42 +0200

    Added Ex05

---
+++ Ex05/CG1.Ex05/Structure/Face.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Drawing;
+using CG1.Ex05.Mathematics;
+
+namespace CG1.Ex05.Structure
+{
+    public class MeshFace
+    {
+        #region Properties
+
+        /// <summary>
+        /// One of the Bounding half edges.
+        /// </summary>
+        public HalfEdge Edge { get; set; }
+
+        /// <summary>
+        /// Center point of face. Maintained just for picking.
+        /// </summary>
+        private Vector4 center;
+
+        /// <summary>
+        /// Maintained just for picking.
+        /// </summary>
+        public Boolean IsSelected;
+
+        #endregion
+
+        #region Get and Set Methods
+
+        public Vector4 Center
+        {
+            get { return center; }
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        /// <summary>
+        /// Geometric center of standard convex polygon without holes.
+        /// </summary>
+        public void SetCenter()
+        {
+            center = new Vector4(0, 0, 0);
+            //ToDo: Compute geometric center of polygon.
+            //      You can go through all vertices of polygon and compute. 
+            //      Remember that vertices of polygon are at a same time vectors - you can use operations for vectors.
+            //Hint: Add all polygon vertices and then divide them by number of polygon vertices
+            //      You can use method Vertices
+            int count = 0;
+        }
+
+        /// <summary>
+        /// Return all vertices of polygon as Points. You can use it during drawing and during computation of polygon's center.
+        /// </summary>
+        public IEnumerable<Point> Vertices()
+        {
+            //ToDo: Return all vertices of current face. Use navigation theory of half-edge data structure.
+            //      You should return them as Points - used in drawing of polygon in method Draw(Graphics g).
+            yield return new Point();
+        }
+
+        #endregion
+    }
+}
---
+++ Ex05/CG1.Ex05/Structure/HalfEdge.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CG1.Ex05.Mathematics;
+
+namespace CG1.Ex05.Structure
+{
+    //Info: For more information about half-edge you can use f.e. http://goo.gl/yz2vp
+    //      How to navigate through the mesh? - http://goo.gl/zh91H
+    public class HalfEdge
+    {
+        #region Properties
+
+        /// <summary>
+        /// Vertex this edge points to
+        /// </summary>
+        public MeshVertex Direction { get; set; }
+
+        /// <summary>
+        /// Vertex this edge will leave. This is not in default structure and theory. It is used for easier acces to mesh.
+        /// </summary>
+        public MeshVertex Origin { get; set; }
+
+        /// <summary>
+        /// The face it belongs to
+        /// </summary>
+        public MeshFace Face { get; set; }
+
+        /// <summary>
+        /// The opposite half edge
+        /// </summary>
+        public HalfEdge Opposite { get; set; }
+
+        /// <summary>
+        /// The next half edge inside the face (ordered counter-clockwise)
+        /// </summary>
+        public HalfEdge Next { get; set; }
+
+        /// <summary>
+        /// Center of the half edge.
+        /// </summary>
+        private Vector4 center;
+
+        /// <summary>
+        /// Maintained just for picking.
+        /// </summary>
+        public Boolean IsSelected;
+
+        #endregion
+
+        #region Constructors
+
+        public HalfEdge() { }
+
+        public HalfEdge(MeshVertex origin, MeshVertex direction, MeshFace face, HalfEdge twin, HalfEdge next)
+        {
+            Origin = origin;
+            Direction = direction;
+            Face = face;
+            Opposite = twin;
+            Next = next;
+        }
+
+        #endregion
+
+        #region Get and Set Methods
+
+        public Vector4 Center
+        {
+            get { return center; }
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        public void SetCenter()
+        {
+            //ToDo: Compute center of current edge. Used in picking and drawing in method TrySelect() and Draw()
+            center = Vector4.Zero;
+        }
+
+        #endregion
+    }
+}
